bb251e8074b324d98f13b591a031baca42147711
Maycon <57063866+MayconL27@users.noreply.github.com>
2022-03-23 12:28:22 -0300

    Calcular media de notas

---
+++ 1-Media-Notas/script.js
@@ -0,0 +1,10 @@
+function calcular() {
+    var n1 = document.getElementById('n1').value;
+    var n2 = document.getElementById('n2').value;
+    var n3 = document.getElementById('n3').value;
+    var n4 = document.getElementById('n4').value;
+
+    var media = (Number(n1) + Number(n2) + Number(n3) + Number(n4)) / 4;
+
+    alert(`A media dos valores é: ${media}`);
+}
