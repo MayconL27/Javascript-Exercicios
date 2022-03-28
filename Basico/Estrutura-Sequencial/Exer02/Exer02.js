28c92bca27648200963bd2969530078c9158f9f4
Maycon <57063866+MayconL27@users.noreply.github.com>
2022-03-28 20:30:11 -0300

    Correções

--- Basico/Estrutura-Sequencial/Exer02/Exer02.js
+++
@@ -1,6 +0,0 @@
-// Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
-var capturando = ""
-function capturar() {
-    capturando = document.querySelector('input#valor').value
-    document.getElementById('valorDigitado').innerHTML = capturando  
-}
