95b030049c9d47d9243994edd5f8e08074edece6
MayconL27 <mayconleandro2008@gmail.com>
2022-04-14 11:56:25 -0300

    Exercicio completo

--- Exercicios-js-Curso-Em-Video/Verificador-De-Idade/script.js
+++ Exercicios-js-Curso-Em-Video/Verificador-De-Idade/script.js
@@ -3,12 +3,43 @@ function verificar() {
     var ano = data.getFullYear()
     var fano = document.querySelector('input#ano')
     var res = document.querySelector('div#res')
+
     if (fano.value.length == 0 || Number(fano.value) > ano) {
-        alert('Erro')
-        
+        alert('Erro')    
     } else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
-        res.innerHTML = `Idade calculada: ${idade} anos` 
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id','foto')
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if (idade >=0 && idade < 18) {
+                //Jovem
+                img.setAttribute('src','img/jovemH.png')
+            } else if (idade < 60) {
+                //Adulto
+                img.setAttribute('src','img/adultoH.png')
+            } else {
+                //Idoso
+                img.setAttribute('src','img/idosoH.png')
+            }
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+            if (idade >=0 && idade < 18) {
+                //Jovem
+                img.setAttribute('src','img/jovemM.png')
+            } else if ( idade < 60) {
+                //Adulto
+                img.setAttribute('src','img/adultoM.png')
+            } else {
+                //Idoso
+                img.setAttribute('src','img/idosoM.png')
+            }
+        }
+        res.style.textAlign = 'center' // centralizar texto do resultado
+        res.innerHTML = `${gênero} com ${idade} anos`
+        res.appendChild(img)
+        
     }
 }
