2f956a5627cfa62f5cd69ce1af03ba1a7b5d92f6
MayconL27 <mayconleandro2008@gmail.com>
2022-04-21 14:48:24 -0300

    Exercicio contagem incompleto

---
+++ Exercicios-js-Curso-Em-Video/Contagem/script.js
@@ -0,0 +1,11 @@
+function calcular() {
+    var inicio = document.querySelector('#inicio').value
+    var fim = document.querySelector('#fim').value
+    var passos = document.querySelector('#passos').value
+
+    for (let index = 1; index <= fim; index++) {
+        
+        alert(index)
+    }
+    
+}
