d304df755bc0505b1bec701ac99a0c91a621e3ff
MayconL27 <mayconleandro2008@gmail.com>
2022-04-22 16:30:16 -0300

    Exercicio Contagem

--- Exercicios-js-Curso-Em-Video/Contagem/script.js
+++ Exercicios-js-Curso-Em-Video/Contagem/script.js
@@ -1,11 +1,32 @@
-function calcular() {
-    var inicio = document.querySelector('#inicio').value
-    var fim = document.querySelector('#fim').value
-    var passos = document.querySelector('#passos').value
-
-    for (let index = 1; index <= fim; index++) {
-        
-        alert(index)
-    }
+function contar() {
+    var inicio = document.querySelector('input#inicio')
+    var fim = document.querySelector('input#fim')
+    var passos = document.querySelector('input#passos')
+    var res = document.querySelector('div#res')
     
+
+    if (inicio.value.length == 0 || fim.value.length == 0 || passos.value.length == 0) {
+        res.innerHTML = 'Impossivel contar, Faltam dados!'
+    } else {
+        res.innerHTML = 'contando: '
+        var i = Number(inicio.value)
+        var f = Number(fim.value)
+        var p = Number(passos.value)
+        if (p <= 0) {
+            alert('Passo inválido!')  
+            p = 1 
+        }
+        if (i < f) {
+            // Contagem crescente
+            for(var c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c}`
+            }
+        } else {
+            // Contagem regressiva
+            for(var c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c}`
+            }
+        }
+       
+    }    
 }
